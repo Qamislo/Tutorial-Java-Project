a0185073e1035ef37ce7118f04cd60da5c6f441e
Ado <ado8245@hotmail.com>
2018-03-24 14:13:36 +0200

    add Hello World Class

---
+++ src/tutorialjavaproject/HelloWorldClass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tutorialjavaproject;
+
+/**
+ *
+ * @author OS
+ */
+public class HelloWorldClass {
+    
+    void printHello(){
+        System.out.println("Hello World lesson 1.01");
+    }
+    
+}
